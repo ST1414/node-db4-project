713204bca4c08fc9d95aa0dce9b6d1d0bfc1c17d
ST1414 <sterry1414@gmail.com>
2021-12-09 13:32:16 -0800

    Filled out fields for all tables

--- data/migrations/20211209204305_recipe-book.js
+++ data/migrations/20211209204305_recipe-book.js
@@ -3,15 +3,39 @@ exports.up = async function(knex) {
     await knex.schema
         .createTable('recipes', table => {
             table.increments('recipe_id')
+            table.string('recipe_name').notNullable().unique()
+            table.datetime('created_at')
         })
         .createTable('steps', table => {
             table.increments('step_id')
+            table.integer('step_number').notNullable()
+            table.string('step_instructions').notNullable()
+            table.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('recipe_id')
+                .inTable('recipes')
+                .onDelete('RESTRICT')
         })
         .createTable('ingredients', table => {
             table.increments('ingredient_id')
+            table.string('ingredient_name').notNullable().unique();
         })
-        .create('steps_ingredients', table => {
+        .createTable('steps_ingredients', table => {
             table.increments('step_ingredient_id')
+            table.integer('ingredient_id')
+                .unsigned()
+                .notNullable()
+                .references('ingredient_id')
+                .inTable('ingredients')
+                .onDelete('RESTRICT')
+            table.integer('step_id')
+                .unsigned()
+                .notNullable()
+                .references('step_id')
+                .inTable('steps')
+                .onDelete('RESTRICT')
+            table.float('quantity').notNullable().unsigned()
         })
 };
 
