6c95647c0339e35c8e3626f0b89875b2b2204c2d
ST1414 <sterry1414@gmail.com>
2021-12-09 13:11:26 -0800

    Stubbed out the migration file

--- data/migrations/20211209204305_recipe-book.js
+++ data/migrations/20211209204305_recipe-book.js
@@ -1,8 +1,34 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+    await knex.schema
+        .createTable('recipes', table => {
+            table.increments('recipe_id')
+        })
+        .createTable('steps', table => {
+            table.increments('step_id')
+        })
+        .createTable('ingredients', table => {
+            table.increments('ingredient_id')
+        })
+        .create('steps_ingredients', table => {
+            table.increments('step_ingredient_id')
+        })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+
+    await knex.schema
+        .create('steps_ingredients', table => {
+            table.increments('step_ingredient_id')
+        })
+        .createTable('ingredients', table => {
+            table.increments('ingredient_id')
+        })
+        .createTable('steps', table => {
+            table.increments('step_id')
+        })
+        .createTable('recipes', table => {
+            table.increments('recipe_id')
+        })
+
 };
